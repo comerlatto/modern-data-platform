247687307abb98c4eb1d8e59e31e9b1dda342388
comerlatto <comerlatto.thiago@gmail.com>
2026-07-29 22:23:04 -0300

    refactor: add explicit types to sales order staging

--- dbt/adventure_works/models/staging/stg_salesorderheader.sql
+++ dbt/adventure_works/models/staging/stg_salesorderheader.sql
@@ -5,40 +5,40 @@ with source as (
 
 ),
 
-renamed as (
+typed_and_renamed as (
 
     select
-        salesorderid as sales_order_id,
-        revisionnumber as revision_number,
-        orderdate as order_date,
-        duedate as due_date,
-        shipdate as ship_date,
-        status as order_status,
-        onlineorderflag as is_online_order,
-        purchaseordernumber as purchase_order_number,
-        accountnumber as account_number,
-        customerid as customer_id,
-        salespersonid as sales_person_id,
-        territoryid as territory_id,
-        billtoaddressid as bill_to_address_id,
-        shiptoaddressid as ship_to_address_id,
-        shipmethodid as ship_method_id,
-        creditcardid as credit_card_id,
-        creditcardapprovalcode as credit_card_approval_code,
-        currencyrateid as currency_rate_id,
-        subtotal,
-        taxamt as tax_amount,
-        freight,
-        totaldue as total_due,
-        comment,
-        rowguid as row_guid,
-        modifieddate as modified_at,
-        _loaded_at,
-        _source_table
+        cast(salesorderid as integer) as sales_order_id,
+        cast(revisionnumber as smallint) as revision_number,
+        cast(orderdate as timestamp) as order_date,
+        cast(duedate as timestamp) as due_date,
+        cast(shipdate as timestamp) as ship_date,
+        cast(status as smallint) as order_status,
+        cast(onlineorderflag as boolean) as is_online_order,
+        cast(purchaseordernumber as varchar(25)) as purchase_order_number,
+        cast(accountnumber as varchar(15)) as account_number,
+        cast(customerid as integer) as customer_id,
+        cast(salespersonid as integer) as sales_person_id,
+        cast(territoryid as integer) as territory_id,
+        cast(billtoaddressid as integer) as bill_to_address_id,
+        cast(shiptoaddressid as integer) as ship_to_address_id,
+        cast(shipmethodid as integer) as ship_method_id,
+        cast(creditcardid as integer) as credit_card_id,
+        cast(creditcardapprovalcode as varchar(15)) as credit_card_approval_code,
+        cast(currencyrateid as integer) as currency_rate_id,
+        cast(subtotal as numeric(19, 4)) as subtotal,
+        cast(taxamt as numeric(19, 4)) as tax_amount,
+        cast(freight as numeric(19, 4)) as freight,
+        cast(totaldue as numeric(19, 4)) as total_due,
+        cast(comment as varchar(128)) as comment,
+        cast(rowguid as uuid) as row_guid,
+        cast(modifieddate as timestamp) as modified_at,
+        cast(_loaded_at as timestamptz) as _loaded_at,
+        cast(_source_table as text) as _source_table
 
     from source
 
 )
 
 select *
-from renamed
+from typed_and_renamed
