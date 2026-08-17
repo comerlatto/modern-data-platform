ae6ad29913de0eb2c7779bcf683618df50eac388
comerlatto <comerlatto.thiago@gmail.com>
2026-08-17 20:25:10 -0300

    feat: add monthly sales fact

---
+++ dbt/adventure_works/tests/assert_fct_sales_monthly_metrics_are_valid.sql
@@ -0,0 +1,10 @@
+select *
+from {{ ref('fct_sales_monthly') }}
+where
+    orders_count < 0
+    or customers_count < 0
+    or units_sold < 0
+    or gross_sales < 0
+    or discount_amount < 0
+    or net_sales < 0
+    or average_order_value < 0
